d6e6f78e543fc4375468e83766de6f072e923934
YX2022 <yxr371@uregina.ca>
2022-10-13 22:19:47 -0600

    confusing

--- Lab5/script.js
+++ Lab5/script.js
@@ -1,34 +1,15 @@
-function add(){
+$(document).ready(function(){
+    $("#add-button").click(function(){
+        var toAdd = $("input[id=userInput]").val();
+        $('ul').append('<li>' + toAdd + '</li>');
+   });
+});
 
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("userInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("userInput").value = "";
-}
-function sort(){
-  var list = document.getElementById("myUL");
-  var switching = true;
-  while (switching) {
-    switching = false;
-    var b = list.getElementsByTagName("LI");
-    for (i = 0; i < (b.length - 1); i++) {
-      var shouldSwitch = false;
-      if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
-        var shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      b[i].parentNode.insertBefore(b[i + 1], b[i]);
-      switching = true;
-    }
+$(document).ready(function(){
+    $("#remove-button").click(function(){
+        $("#").empty();
+        
+    })
+})
 
-}
-}
 
