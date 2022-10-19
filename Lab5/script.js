4fa1c928f773fea6880e4a5dc74e2c47226ef214
YX2022 <yxr371@uregina.ca>
2022-10-18 20:20:15 -0600

    1

--- Lab5/script.js
+++ Lab5/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
     $("#add-button").click(function(){
-        var toAdd = $("input[id=userInput]").val();
-        $('ul').append('<li>' + toAdd + '</li>');
+        var inputValue = $("input[id=userInput]").val();
+        $("ul").append('<li>' + inputValue + '</li>');
    });
 });
 
 $(document).ready(function(){
     $("#remove-button").click(function(){
         $("#").empty();
-        
+
     })
 })
 
