0b8131b0556bbf9ca7717b1c8bd1991ac7266db2
YX2022 <yxr371@uregina.ca>
2022-11-13 16:49:36 -0600

    pre for lab9

--- Lab9/app.js
+++ Lab9/app.js
@@ -1,3 +1,58 @@
+// Bring in mongoose
+const mongoose = require( 'mongoose' );
+
+// connects to the "test" database (ensure mongod is running!)
+// the later arguments fix some deprecation warnings
+mongoose.connect( 'mongodb://localhost:27017/test', 
+                  { useNewUrlParser: true, useUnifiedTopology: true });
+
+// create a schema
+//const Cat = mongoose.model( 'Cat', { name: String });
+
+// create a document following that schema
+//const kitty = new Cat({ name: 'Zildjian' });
+//  kitty.save().then( () => console.log('meow') );
+const userSchema = new mongoose.Schema ({
+    username: String,
+    password: String,
+});
+
+const taskSchema = new mongoose.Schema({
+    text: String,
+    state: String,
+    creator: String,
+    isTaskClaimed:Boolean,
+    claimingUser: String,
+    isTaskDone: Boolean,
+    isTaskCleared: Boolean
+});
+// this creates a collection called `games` (Weird, but intuitive)
+const User = mongoose.model ( "User", userSchema );
+const Task = mongoose.model ( "Task", taskSchema );
+
+app.post("/submit",(req, res )=>{
+    console.log("A user is logging in");
+    console.log(req.body);
+
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+user.save();
+res.redirect("/todo")
+});
+
+app.get("/",async(req,res)=>{
+    console.log("A user is logging in ")
+})
+
+
+//game.save();
+
+
+
+
+
 //  const fs = require( "fs" );
 
 // var myObj = { name : "yxr371" ,
@@ -66,3 +121,7 @@ app.listen (port, () => {
 app.get("/", (req, res) =>{
     res.sendFile(__dirname + "/public/login.html");
 });
+
+
+
+require("dotenv").config();
