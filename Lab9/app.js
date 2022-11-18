d7cacbe4ac0e28efb7f652256b54ecaf52a9005a
YX2022 <yxr371@uregina.ca>
2022-11-17 20:49:18 -0600

    updated more

--- Lab9/app.js
+++ Lab9/app.js
@@ -1,136 +1,17 @@
-// Bring in mongoose
-const mongoose = require( 'mongoose' );
-
-// connects to the "test" database (ensure mongod is running!)
-// the later arguments fix some deprecation warnings
-mongoose.connect( 'mongodb://localhost:27017/test', 
-                  { useNewUrlParser: true, useUnifiedTopology: true });
-
-// create a schema
-//const Cat = mongoose.model( 'Cat', { name: String });
-
-// create a document following that schema
-//const kitty = new Cat({ name: 'Zildjian' });
-//  kitty.save().then( () => console.log('meow') );
-//const userSchema = new mongoose.Schema ({
- //   username: String,
- //   password: String,
-//});
-
-//const User = mongoose.model ( "User", userSchema );
-
-//const user = new User({
-//     username: "yxr371",
-//     password: "123321"
-// });
-const taskSchema = new mongoose.Schema({
-    _id : Number,
-    text: String,
-    state: String,
-    creator: String,
-    isTaskClaimed: Boolean,
-    claimingUser: String,
-    isTaskDone: Boolean,
-    isTaskCleared: Boolean
-});
-// this creates a collection called `games` (Weird, but intuitive)
-
-const Task = mongoose.model ( "Task", taskSchema );
-
-app.post("/submit",(req, res )=>{
-    console.log("A user is logging in");
-    console.log(req.body);
-
-res.redirect("/todo")
-});
-
-app.get("/",async(req,res)=>{
-    console.log("A user is logging in ")
-})
-
-
-//game.save();
-
-/*                                                                         */
-
-
-
-//  const fs = require( "fs" );
-
-// var myObj = { name : "yxr371" ,
-//             password:"123321"};
-
-// fs.writeFile ( __dirname + "/object.json", 
-//                    JSON.stringify( myObj ), 
-//                    "utf8", 
-//                    ( err ) => {
-//     if ( err ) {
-//         console.log( "Error writingthe file:", err );
-//         return;
-//     }
-// });
-
-
-const fs = require( "fs" );
-// var myObj = { username : "yxr371" ,
-//             password:"123321"};
-// fs.writeFile ( __dirname + "/object.json", 
-//                    JSON.stringify( myObj ), 
-//                    "utf8", 
-//                    ( err ) => {
-//     if ( err ) {
-//         console.log( "Error writingthe file:", err );
-//         return;
-//     }
-// });
-
-fs.readFile ( __dirname + "/object.json",
-            "utf8", 
-            ( err, jsonString ) => {
-    if ( err ) {
-        console.log("Error reading file from disk:", err);
-        return;
-    }
-    try {
-        const object = JSON.parse(jsonString);
-        app.post("/", (req, res) => {
-            if(req.body["my-name"] == object.username  && req.body["my-pswd"] == object.password ){
-                res.sendFile(__dirname + "/public/todo.html")
-            }
-            else{
-                console.log("wrong username or password");
-                res.redirect("/");
-            }
-          });
-        
-    } catch ( err ) {
-        console.log("Error parsing JSON:", err);
-    }
-});
-
-
-const express = require ( "express" );
-const app = express(); 
-app.use(express.urlencoded({ extended: true})); 
-app.use(express.static("public"));
-app.set("view engine","ejs");
-const port = 3000; 
-
-app.listen (port, () => {
-    console.log (`Server is running on http://localhost:${port}`);
-});
-
-app.get("/", (req, res) =>{
-    res.sendFile(__dirname + "/public/login.html");
-});
-
-
-
 
 DB_HOST=localhost
 DB_USER=root
 
 process.env.DB_HOST
+
+const express = require ( "express" );
+const mongoose = require( 'mongoose' );
+const app = express(); 
+
+const port = 3000; 
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
 
 ////1. Require dependencies//////////////////////////////////////////////////////////////////////
 const session = require("express-session")
@@ -149,20 +30,39 @@ app.use(session({
 app.use (passport.initialize());
 app.use (passport.session());
 
+app.set("view engine","ejs");
+
+mongoose.connect( 'mongodb://localhost:27017/test', 
+                  { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 // 3. Create the userSchema /////////////////////////////////////////
 // It is important not to change these names
 // passport-local-mongoose expects these. Use `username` and `password`!
+const taskSchema = new mongoose.Schema({
+    _id : Number,
+    text: String,
+    state: String,
+    creator: String,
+    isTaskClaimed: Boolean,
+    claimingUser: String,
+    isTaskDone: Boolean,
+    isTaskCleared: Boolean
+});
+
+const Task = mongoose.model ( "Task", taskSchema );
+
+
+
 const userSchema = new mongoose.Schema ({
     username: String,
     password: String
 })
-
 // plugins extend Schema functionality
 userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 ////////////////////////////////////////////////////////////////////
-
 
 // 4. Add our strategy for using Passport, using the local user from MongoDB
 passport.use(User.createStrategy());
@@ -170,7 +70,9 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+app.listen (port, () => {
+    console.log (`Server is running on http://localhost:${port}`);
+});
 // 5. Register a user with the following code, which needs to be in the appropriate route
 // As in (3), be sure to use req.body.username and req.body.password, and ensure the 
 // html forms match these values as well
@@ -184,7 +86,7 @@ app.post( "/register", (req, res) => {
             res.redirect( "/" );
         } else {
             passport.authenticate( "local" )( req, res, () => {
-                res.redirect( "/reviews" );
+                res.redirect( "/todo" );
             });
         }
     });
@@ -204,20 +106,25 @@ app.post( "/login", ( req, res ) => {
             res.redirect( "/" );
         } else {
             passport.authenticate( "local" )( req, res, () => {
-                res.redirect( "/reviews" ); 
+                res.redirect( "/todo" ); 
             });
         }
     });
 });
+
+app.get("/", (req, res) =>{
+    console.log("A user is accessing the root route using get");
+    res.sendFile(__dirname + "/public/index.html");
+});
 ////////////////////////////////////////////////////////////////////
 
 // 7. Register get routes for reviews and add-review ////////////////
-app.get( "/reviews", async( req, res ) => {
-    console.log("A user is accessing the reviews route using get, and...");
+app.get( "/todo", async( req, res ) => {
+    console.log("A user is accessing the todo route using get, and...");
     if ( req.isAuthenticated() ){
         try {
             console.log( "was authorized and found:" );
-            const results = await Game.find();
+            const results = await Task.find();
             console.log( results );
             res.render( "reviews", { results : results });
         } catch ( error ) {
@@ -229,7 +136,6 @@ app.get( "/reviews", async( req, res ) => {
     }
 });
 
-// 7. Register get routes for reviews and add-review ////////////////
 app.get( "/add-review", ( req, res ) => {
     console.log( "A user is accessing the add-review page, and..." );
     if (req.isAuthenticated()) {
@@ -240,6 +146,7 @@ app.get( "/add-review", ( req, res ) => {
         res.redirect( "/" ) 
     }
 });
+
 ////////////////////////////////////////////////////////////////////
 
 // 8. Logout ///////////////////////////////////////////////////////
@@ -271,6 +178,91 @@ app.post( "/submit", async( req, res ) => {
     res.redirect( "/reviews" );
 });
 ////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+// Bring in mongoose
+
+// connects to the "test" database (ensure mongod is running!)
+// the later arguments fix some deprecation warnings
+
+// create a schema
+//const Cat = mongoose.model( 'Cat', { name: String });
+
+// create a document following that schema
+//const kitty = new Cat({ name: 'Zildjian' });
+//  kitty.save().then( () => console.log('meow') );
+//const userSchema = new mongoose.Schema ({
+ //   username: String,
+ //   password: String,
+//});
+
+//const User = mongoose.model ( "User", userSchema );
+
+//const user = new User({
+//     username: "yxr371",
+//     password: "123321"
+// });
+
+// this creates a collection called `games` (Weird, but intuitive)
+
+//game.save();
+
+//  const fs = require( "fs" );
+
+// var myObj = { name : "yxr371" ,
+//             password:"123321"};
+
+// fs.writeFile ( __dirname + "/object.json", 
+//                    JSON.stringify( myObj ), 
+//                    "utf8", 
+//                    ( err ) => {
+//     if ( err ) {
+//         console.log( "Error writingthe file:", err );
+//         return;
+//     }
+// });
+
+
+//const fs = require( "fs" );
+// var myObj = { username : "yxr371" ,
+//             password:"123321"};
+// fs.writeFile ( __dirname + "/object.json", 
+//                    JSON.stringify( myObj ), 
+//                    "utf8", 
+//                    ( err ) => {
+//     if ( err ) {
+//         console.log( "Error writingthe file:", err );
+//         return;
+//     }
+// });
+
+// fs.readFile ( __dirname + "/object.json",
+//             "utf8", 
+//             ( err, jsonString ) => {
+//     if ( err ) {
+//         console.log("Error reading file from disk:", err);
+//         return;
+//     }
+//     try {
+//         const object = JSON.parse(jsonString);
+//         app.post("/", (req, res) => {
+//             if(req.body["my-name"] == object.username  && req.body["my-pswd"] == object.password ){
+//                 res.sendFile(__dirname + "/public/todo.html")
+//             }
+//             else{
+//                 console.log("wrong username or password");
+//                 res.redirect("/");
+//             }
+//           });
+        
+//     } catch ( err ) {
+//         console.log("Error parsing JSON:", err);
+//     }
+// });
 
 
 
